dd91cfb675d1f307994a851aa4746b67ccc1efcc
Excel404 <65623910+Excel404@users.noreply.github.com>
2021-07-30 21:28:38 +0100

    first update

--- chatbot.js
+++ chatbot.js
@@ -1,4 +1,5 @@
 let inputMessage = document.getElementById('inputBox');
+
 function MessageBox(){
     let chatArea = document.getElementById('chatArea');
     
@@ -17,40 +18,47 @@ function MessageBox(){
             chatArea.appendChild(Box);
             chatArea.appendChild(space);
             chatArea.appendChild(space);
-            console.log(inputMessage.value.toLowerCase());
         }
+        
 
     }
-    let myInput = ["Hello", "How are you doing", "Who created you?", "Ok, that's good"];
+    let myInput = ["hello", "how are you doing", "who created you?", "ok, that's good"];
     let replies = ["Hi, I'm Jarvis", "I'm fine", "Excel","Goodbye"];
     let hiddenDiv = document.getElementById("moreDisplay");
     this.addWords = ()=>{
-      hiddenDiv.removeAttribute("hidden");
-    }
-    this.close = ()=>{
-      hiddenDiv.hidden= true;
-      console.log("working");
+      hiddenDiv.classList.toggle("show");
+      let Input = document.getElementById("Input");
+      let Reply = document.getElementById("reply")
+      
+      this.addInput = ()=>{
+        myInput.push(Input.value);
+        console.log(myInput)
+      }
+      this.addReply = () =>{
+        replies.push(Reply.value);
+        console.log(replies)
+      }
+      
     }
     this.checker = ()=>{
         
         for (var i = 0; i< myInput.length ;i++){
             if(inputMessage.value != ""){
-                if (myInput[i].toLowerCase().includes(inputMessage.value)){
+                if (myInput[i].includes(inputMessage.value.toLowerCase())){
                     this.createMessage(replies[i],'white', 'left');
                 }
+             
             }
+            
 
         }
+       inputMessage.value = "";
     }
-
-
 }
-let user = new MessageBox();
-let Jarvis = new MessageBox();
+
 function execute(){
-    console.log('check');
-    user.createMessage(inputMessage.value,"lightgreen","right");
-    Jarvis.checker()
+  let user = new MessageBox();
+  let Jarvis = new MessageBox();
+  user.createMessage(inputMessage.value,"lightgreen","right");
+  Jarvis.checker()
 }
-
-console.log(user);
